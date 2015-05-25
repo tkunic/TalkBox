57adb4761e7e4adc7e784f0126a4b08dfcfd289c
Brandon Haworth <brandon@cse.yorku.ca>
2015-05-25 18:20:41 +0000

    Rewrite talkbox startup script

--- daemon/talkbox.sh
+++ daemon/talkbox.sh
@@ -1,58 +1,34 @@
-#!/bin/sh
+#! /bin/sh
+# /etc/init.d/talkbox.sh
  
 ### BEGIN INIT INFO
-# Provides:          talkbox
-# Required-Start:    $remote_fs $syslog
-# Required-Stop:     $remote_fs $syslog
-# Default-Start:     2 3 4 5
-# Default-Stop:      0 1 6
-# Short-Description: Runs TalkBox as daemon when the pi is powered on.
-# Description:       Runs TalkBox as daemon when the pi is powered on.
+# Provides: talkbox
+# Required-Start: $remote_fs $syslog
+# Required-Stop: $remote_fs $syslog
+# Default-Start: 2 3 4 5
+# Default-Stop: 0 1 6
+# Short-Description: Simple script to start a program at boot
+# Description: A simple script from www.stuffaboutcode.com which will start / stop a program a boot / shutdown.
 ### END INIT INFO
+   
+# If you want a command to always run, put it here
  
-# Change the next 3 lines to suit where you install your script and what you want to call it
-DIR=/home/pi/TalkBox
-DAEMON=$DIR/talkbox.py
-DAEMON_NAME=talkboxd
- 
-# This next line determines what user the script runs as.
-# Root generally not recommended but necessary if you are using the Raspberry Pi GPIO from Python.
-DAEMON_USER=root
- 
-# The process ID of the script when it runs is stored here:
-PIDFILE=/var/run/$DAEMON_NAME.pid
- 
-. /lib/lsb/init-functions
- 
-do_start () {
-    log_daemon_msg "Starting system $DAEMON_NAME daemon"
-    start-stop-daemon --start --background --pidfile $PIDFILE --make-pidfile --user $DAEMON_USER --chuid $DAEMON_USER --startas $DAEMON
-    log_end_msg $?
-}
-do_stop () {
-    log_daemon_msg "Stopping system $DAEMON_NAME daemon"
-    start-stop-daemon --stop --pidfile $PIDFILE --retry 10
-    log_end_msg $?
-}
- 
+# Carry out specific functions when asked to by the system
 case "$1" in
- 
-    start|stop)
-        do_${1}
-        ;;
- 
-    restart|reload|force-reload)
-        do_stop
-        do_start
-        ;;
- 
-    status)
-        status_of_proc "$DAEMON_NAME" "$DAEMON" && exit 0 || exit $?
-        ;;
-    *)
-        echo "Usage: /etc/init.d/$DAEMON_NAME {start|stop|restart|status}"
-        exit 1
-        ;;
- 
+start)
+echo "Starting TalkBox..."
+# run application you want to start
+python /home/pi/TalkBox/talkbox.py
+;;
+stop)
+echo "Stopping TalkBox..."
+# kill application you want to stop
+killall python
+;;
+*)
+echo "Usage: /etc/init.d/talkbox.sh {start|stop}"
+exit 1
+;;
 esac
+ 
 exit 0
